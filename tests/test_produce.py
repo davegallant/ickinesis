ee4151b3f4c0f2f02be35ec1662b09acb61329ae
Dave Gallant <davegallant@gmail.com>
2019-03-13 00:19:07 -0400

    cleanup tests.

--- tests/test_produce.py
+++
@@ -1,8 +0,0 @@
-from kinesis.producer import KinesisProducer
-
-
-def test_put_record(kinesis):
-    kinesis_producer = KinesisProducer(
-        stream_name="test-stream", kinesis_client=kinesis
-    )
-    kinesis_producer.put_record(message="fake message")
